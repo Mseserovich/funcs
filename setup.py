67a5563eba7d57f663a8210e33ec6679d84d9c02
Tshepo Tshabalala <tshepo.tshabalala@moov.life>
2019-03-22 08:07:22 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Some of the algorithms using recursion as well as sorting algorithms',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<smtolo>/<piethorn>',
+    url='https://github.com/<Mseserovich>/<funcs>',
     author='<Tshepo Tshabalala>',
     author_email='<tshepo.tshabalala@moov.life>'
 )
